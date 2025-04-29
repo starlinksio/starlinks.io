b2c8cd89d28895896aba023f84e1e7cf651f0cc4
Uriel Malka <urielmalka1@gmail.com>
2025-04-29 18:10:44 +0300

    implementation entities

---
+++ starlinks-lib/Starlinks.hpp
@@ -0,0 +1,28 @@
+#ifndef STARLINKS_HPP
+#define STARLINKS_HPP
+
+#include "entities/Token.hpp"
+
+class Starlinks
+{
+    private:
+        /* data */
+        Token token;
+        
+
+    public:
+        Starlinks(/* args */);
+        ~Starlinks();
+};
+
+Starlinks::Starlinks(/* args */)
+{
+}
+
+Starlinks::~Starlinks()
+{
+}
+
+
+
+#endif
