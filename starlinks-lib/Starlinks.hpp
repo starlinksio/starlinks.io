a9ded1cd4de477e64a3682a7e3cd34f733870a47
Uriel Malka <urielmalka1@gmail.com>
2025-04-29 18:19:33 +0300

    implementation entities

--- starlinks-lib/Starlinks.hpp
+++ starlinks-lib/Starlinks.hpp
@@ -2,6 +2,14 @@
 #define STARLINKS_HPP
 
 #include "entities/Token.hpp"
+#include "entities/Link.hpp"
+#include "entities/Shortcut.hpp"
+#include "entities/Click.hpp"
+#include "entities/Domain.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
 
 class Starlinks
 {
@@ -13,6 +21,23 @@ class Starlinks
     public:
         Starlinks(/* args */);
         ~Starlinks();
+
+        Link* create_link();
+        Shortcut* add_shortcut();
+
+        bool delete_link();
+        bool delete_shorcut();
+
+        string get_token() { return token.token; }
+        
+        vector<Domain> get_domains();
+
+        vector<Link> get_links();
+        vector<Shortcut> get_shortcuts(Link *link);
+        vector<Click> get_clicks();
+
+
+
 };
 
 Starlinks::Starlinks(/* args */)
